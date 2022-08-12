4d430737dad2befd1d1e3fa01debdaf6771f3247
ataran <andrew@1worldonline.com>
2022-08-12 19:37:46 +0300

    passing historical bpi test case

--- src/main/java/com/kuehnenagel/crypto/price/api/bitcoin/BitcoinPriceApiAdapter.java
+++ src/main/java/com/kuehnenagel/crypto/price/api/bitcoin/BitcoinPriceApiAdapter.java
@@ -5,11 +5,15 @@ import com.kuehnenagel.crypto.price.api.PriceApiAdapter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
 import static java.lang.String.format;
+import static java.util.Collections.emptyList;
 import static java.util.Objects.isNull;
 
 @RequiredArgsConstructor
@@ -17,6 +21,7 @@ public class BitcoinPriceApiAdapter implements PriceApiAdapter {
     private final RestTemplate restTemplate;
     private final DateService dateService;
     private final String baseUrl = "https://api.coindesk.com/v1/bpi/currentprice/";//todo move to config
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault());
 
 
     @Override
@@ -33,6 +38,20 @@ public class BitcoinPriceApiAdapter implements PriceApiAdapter {
 
     @Override
     public List<Double> getHistoricalPrice(String currency, int days) {
-        return null;
+        ResponseEntity<HistoricalBPI> responseEntity = restTemplate.getForEntity(getHistoricalPriceUrl(currency, days), HistoricalBPI.class);
+        HistoricalBPI responseBody = responseEntity.getBody();
+        if (isNull(responseBody)) return emptyList();
+        return responseBody.getBpi().values().stream().toList();
+    }
+
+    private String getHistoricalPriceUrl(String currency, int days) {
+        String startDate = formatter.format(dateService.getDateForDaysBack(days));
+        String endDate = formatter.format(dateService.getCurrentDate());
+        return UriComponentsBuilder.fromHttpUrl("https://api.coindesk.com/v1/bpi/historical/close.json")
+                .queryParam("start", startDate)
+                .queryParam("end", endDate)
+                .queryParam("currency", currency)
+                .encode()
+                .toUriString();
     }
 }
